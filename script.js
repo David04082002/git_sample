bc710fc776d9ba6224c6bd4d0de80338c407fefd
Giandavid Ferrer <ferrer.237085@alabang.sti.edu.ph>
2023-07-31 15:01:17 +0800

    Added .gitignore

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 
 
-// git config --global alias.s (shortcut term) "status" (actual command)
-// git push origin main --set-upstream -> git push
-
-// to sync changes from github to computer user 
-// git fetch, then
-// git pull origin main --set-upstream -> git pull
-
-// git clone (url) (folder name) - to get a remote repository
-
 console.log('hello world');
 
-console.log('Version 11');
+console.log('Version 14');
 
 /*
 
